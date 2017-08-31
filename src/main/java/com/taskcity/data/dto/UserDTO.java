fcb72037863b760f5cf4931afaaec531aa0d1409
handrinp <handrinp@uwec.edu>
2017-08-31 14:06:56 -0500

    Added ability to get tasks from UserDTO

--- src/main/java/com/taskcity/data/dto/UserDTO.java
+++ src/main/java/com/taskcity/data/dto/UserDTO.java
@@ -1,6 +1,11 @@
 package com.taskcity.data.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.json.JSONObject;
+
+import com.taskcity.data.DataFactory;
 
 public class UserDTO {
 	private String username;
@@ -21,6 +26,15 @@ public class UserDTO {
 
 	public String getBin() {
 		return bin;
+	}
+
+	public List<TaskDTO> loadSortedTasks() {
+		return DataFactory.getInstance()
+				.createTaskDataSource(bin)
+				.getTasks()
+				.stream()
+				.sorted(TaskDTO.COMPARATOR)
+				.collect(Collectors.toList());
 	}
 
 	private static final String JSON_FORMAT = "{\"name\":\"%s\",\"bin\":\"%s\"}";
