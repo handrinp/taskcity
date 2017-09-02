786e2fddff40b7a483f4f228edd2023d4ee01140
Nick Handrick <handrinp@uwec.edu>
2017-09-02 16:03:03 -0400

    added dueString, millisLeft, and isUrgent methods for #25

--- src/main/java/com/taskcity/data/dto/TaskDTO.java
+++ src/main/java/com/taskcity/data/dto/TaskDTO.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 import org.json.JSONObject;
 
 public class TaskDTO {
+	private static final long URGENT_DURATION = 172_800_000L; // 48 hours, in milliseconds
+	private static final long HOURS = 1000L * 60L * 60L;
+	private static final long DAYS = HOURS * 24L;
+	private static final long WEEKS = DAYS * 7L;
+
 	private String id;
 	private String subject;
 	private String description;
@@ -37,8 +42,51 @@ public class TaskDTO {
 		return due;
 	}
 
-	public boolean isOverdue() {
-		return 0 < due && due < System.currentTimeMillis();
+	private boolean hasDueDate() {
+		return due != 0;
+	}
+
+	public String dueString() {
+		StringBuilder dueString = new StringBuilder();
+
+		if (hasDueDate()) {
+			long millis = millisLeft();
+
+			if (millis > 0) {
+				long hours = (millis / HOURS) % 24L;
+				long days = (millis / DAYS) % 7L;
+				long weeks = millis / WEEKS;
+
+				if (weeks != 0) {
+					dueString.append(weeks)
+							.append("w ");
+				}
+
+				if (days != 0) {
+					dueString.append(days)
+							.append("d ");
+				}
+
+				if (hours != 0) {
+					dueString.append(hours)
+							.append("h");
+				}
+			} else {
+				dueString.append("LATE");
+			}
+		}
+
+		return dueString.toString()
+				.trim()
+				.replaceAll(" ", "<br>");
+	}
+
+	private long millisLeft() {
+		return due - System.currentTimeMillis();
+	}
+
+	public boolean isUrgent() {
+		return hasDueDate() && millisLeft() < URGENT_DURATION;
 	}
 
 	private static final String JSON_FORMAT = "{\"subject\":\"%s\",\"task\":\"%s\",\"due\":%d,\"taskid\":\"%s\"}";
@@ -53,6 +101,8 @@ public class TaskDTO {
 		return String.format("Task %s : [subject: %s, description: %s, due: %d]", id, subject, description, due);
 	}
 
-	public static final Comparator<TaskDTO> COMPARATOR = Comparator.comparing(TaskDTO::getSubject)
+	public static final Comparator<TaskDTO> COMPARATOR = Comparator.comparing(TaskDTO::isUrgent)
+			.reversed()
+			.thenComparing(TaskDTO::getSubject)
 			.thenComparingLong(TaskDTO::getDue);
 }
