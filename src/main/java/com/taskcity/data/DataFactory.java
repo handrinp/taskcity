ebe63cadbc4bc910336840976e34f39f0c84dfbb
Nick Handrick <handrinp@uwec.edu>
2017-09-05 18:23:48 -0500

    updated DataFactory to use MySQL data sources

--- src/main/java/com/taskcity/data/DataFactory.java
+++ src/main/java/com/taskcity/data/DataFactory.java
@@ -1,15 +1,9 @@
 package com.taskcity.data;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class DataFactory {
 	private static DataFactory instance;
 
-	private Map<String, TaskDataSource> taskDSCache;
-
 	private DataFactory() {
-		taskDSCache = new HashMap<>();
 	}
 
 	public static DataFactory getInstance() {
@@ -21,18 +15,10 @@ public class DataFactory {
 	}
 
 	public UserDataSource createUserDataSource() {
-		return new MyJSONUserDS();
+		return new MySQLUserDS();
 	}
 
-	public TaskDataSource createTaskDataSource(String bin) {
-		return retrieveTaskDSFromCache(bin);
-	}
-
-	private TaskDataSource retrieveTaskDSFromCache(String bin) {
-		if (!taskDSCache.containsKey(bin)) {
-			taskDSCache.put(bin, new MyJSONTaskDS(bin));
-		}
-
-		return taskDSCache.get(bin);
+	public TaskDataSource createTaskDataSource(String username) {
+		return new MySQLTaskDS(username);
 	}
 }
